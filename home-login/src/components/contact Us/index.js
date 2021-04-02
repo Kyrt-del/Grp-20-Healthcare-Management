40402147f103821e4f6f6598e5e1a11fffff49b1
Devyani-1403 <66554088+Devyani-1403@users.noreply.github.com>
2021-04-02 21:32:47 +0530

    Add files via upload

---
+++ home-login/src/components/contact Us/index.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './style.css'
+
+const Contact = () =>
+{
+    return (
+        <React.Fragment>
+          
+            <h1 className="contact-h1"> Get in touch </h1>
+          <hr class="style13" />
+          <br/>
+          <div class="contact-row">
+              <div>
+                  <p class="contact-text">njdsjkfnoifjoisgjisorngosdnobvisnbesiorbnevisrnves</p>
+
+              </div>
+              <div class="contact">
+                  <h3>Contact Us </h3>
+                  
+                  </div>
+                 
+                    <h5 class="wrapper-info">info.care@gmail.com</h5>
+                    <h5 class="wrapper-info">9824413747</h5>
+        
+
+          </div>
+
+
+        </React.Fragment>
+    )
+}
+export default Contact;
