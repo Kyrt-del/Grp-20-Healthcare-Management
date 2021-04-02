286fe171351e45e2c92f8f223a4a7cbc66f575d0
Devyani-1403 <66554088+Devyani-1403@users.noreply.github.com>
2021-04-02 16:16:14 +0530

    Update logIn.jsx

--- src/SignIn/logIn.jsx
+++ src/SignIn/logIn.jsx
@@ -8,26 +8,27 @@ import {
   NavDropdown,
   Modal,
 } from "react-bootstrap";
-import "./login.css";
-import logo from "./logo.svg";
+import "./SignIn.css";
+import I3 from '../../images/i3.jpg'
+
 
 class Login extends Component {
   render() {
     return (
       <>
-        <img className="image" src={logo} alt={logo} width="60" height="30" />
+        <img className="image" src={I3} alt={I3} width="60" height="30" />
         <h1 className="text">Sign in to continue</h1>
         <Form className="form">
-          <Form.Group controlId="formBasicEmail" className="temp">
-            <Form.Label>Email address</Form.Label>
+          <Form.Group controlId="formBasicEmail" >
+            <Form.Label className="signin-label">Email address</Form.Label>
             <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="text2">
+            <Form.Text className="signin-label">
               We'll never share your email with anyone else.
             </Form.Text>
           </Form.Group>
 
           <Form.Group controlId="formBasicPassword" className="temp">
-            <Form.Label>Password</Form.Label>
+            <Form.Label className="signin-label">Password</Form.Label>
             <Form.Control type="password" placeholder="Password" />
           </Form.Group>
           <Button variant="dark" type="submit">
