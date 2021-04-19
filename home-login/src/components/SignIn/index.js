1761d7f1bb54037319e3038004c642c350738afd
Kyrt-del <68808704+Kyrt-del@users.noreply.github.com>
2021-04-19 15:29:11 +0300

    Sign In with Authentication updates

--- home-login/src/components/SignIn/index.js
+++ home-login/src/components/SignIn/index.js
@@ -1,43 +1,78 @@
 import React, { Component } from "react";
+import { useHistory } from "react-router-dom";
 import {
   Button,
   Navbar,
   Nav,
   Form,
+  Col,
   FormControl,
   NavDropdown,
   Modal,
 } from "react-bootstrap";
 import "./SignIn.css";
 import I3 from '../../images/i3.jpg'
+import { useState } from "react";
 
 
-class Login extends Component {
-  render() {
-    return (
-      <>
-        <img className="image" src={I3} alt={I3} width="60" height="30" />
-        <h1 className="text">Sign in to continue</h1>
-        <Form className="form">
-          <Form.Group controlId="formBasicEmail" >
-            <Form.Label className="signin-label">Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
-            <Form.Text className="signin-label">
-              We'll never share your email with anyone else.
-            </Form.Text>
-          </Form.Group>
+const Signin = () => {
+  let history = useHistory();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  
+  const onLogin = () => {
+    const API_URL = "https://healthcaremanagement.herokuapp.com";
+    console.log();
+    const obj = {
+        email: email,
+        password: password,
+    }
+    console.log(obj);
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(obj)
+  };
+  console.log("clicked");
+  fetch(`${API_URL}/patient/login`, requestOptions)
+      .then(response => {
+        console.log(response);
+        if(response.ok && response.status == 200){
+          history.push("/");
+        }
+        else{
+          alert(response.err.msg);
+        }
+      })
+      
+} 
 
-          <Form.Group controlId="formBasicPassword" className="temp">
-            <Form.Label className="signin-label">Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
-          </Form.Group>
-          <Button variant="dark" type="submit">
-            Submit
+  return (
+    <>
+      <img className="image" src={I3} alt={I3} width="60" height="30" />
+      <h1 className="text">Sign in to continue</h1>
+      <Form className="form">
+          
+          <Form.Row>
+            <Form.Group as={Col} controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" 
+              onChange = {(event) => setEmail(event.target.value)}/>
+            </Form.Group>
+
+            <Form.Group as={Col} controlId="formGridPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" 
+              onChange = {(event) => setPassword(event.target.value)}/>
+            </Form.Group>
+          </Form.Row>
+
+          <Button variant="dark" onClick = {onLogin}>
+            Login
           </Button>
         </Form>
-      </>
-    );
-  }
+    </>
+  );
 }
 
-export default Login;
+export default Signin;
