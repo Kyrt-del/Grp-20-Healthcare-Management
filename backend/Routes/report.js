01d7b99b041faf06338fbbcc235756e4d510f04d
Ayushii12 <45871043+Ayushii12@users.noreply.github.com>
2021-05-06 15:29:21 +0530

    Update report.js

--- backend/Routes/report.js
+++ backend/Routes/report.js
@@ -50,6 +50,141 @@ router.post('/report/pulseRate',
             
 );
 
+
+router.post('/report',
+[
+    check("id", "Please enter a valid id").notEmpty(),
+    check("bloodpressure", "Please enter a valid Blood Pressure").notEmpty()
+],
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const response = {
+                ok: false,
+                data: {
+                },
+                err: {
+                    status: 400,
+                    msg: errors.errors[0].msg
+                }
+            }
+            return res.status(400).send(response);
+        }
+        try{
+                    const report = new Report({
+                    bloodPressure : req.body.bloodpressure,//token.id;
+                    patient: req.body.id,
+                    pulseRate: null,
+                    bloodSugar: null,
+                    temperature: null,
+                })
+                const reportsaved = await report.save()
+                res.send({
+                    msg: "Succsess",
+                    product: reportsaved
+                });
+            }
+            catch (error) {
+                console.error(err)
+                response.status(500).send({
+                    msg: err.message
+                });
+            }
+            }
+            
+)
+
+
+router.post('/report',
+[
+    check("id", "Please enter a valid id").notEmpty(),
+    check("bloodsugar", "Please enter a valid Blood Sugar").notEmpty()
+],
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const response = {
+                ok: false,
+                data: {
+                },
+                err: {
+                    status: 400,
+                    msg: errors.errors[0].msg
+                }
+            }
+            return res.status(400).send(response);
+        }
+        try{
+                    const report = new Report({
+                    bloodSugar : req.body.bloodsugar,//token.id;
+                    patient: req.body.id,
+                    bloodPressure: [],
+                    pulseRate: null,
+                    temperature: null,
+                })
+                const reportsaved = await report.save()
+                res.send({
+                    msg: "Succsess",
+                    product: reportsaved
+                });
+            }
+            catch (error) {
+                console.error(err)
+                response.status(500).send({
+                    msg: err.message
+                });
+            }
+            }
+            
+)
+
+
+router.post('/report',
+[
+    check("id", "Please enter a valid id").notEmpty(),
+    check("temp", "Please enter a valid Temperature").notEmpty()
+],
+    async (req, res) => {
+
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            const response = {
+                ok: false,
+                data: {
+                },
+                err: {
+                    status: 400,
+                    msg: errors.errors[0].msg
+                }
+            }
+            return res.status(400).send(response);
+        }
+        try{
+                    const report = new Report({
+                    temperature : req.body.temp,//token.id;
+                    patient: req.body.id,
+                    bloodPressure: [],
+                    bloodSugar: null,
+                    pulseRate: null,
+                })
+                const reportsaved = await report.save()
+                res.send({
+                    msg: "Succsess",
+                    product: reportsaved
+                });
+            }
+            catch (error) {
+                console.error(err)
+                response.status(500).send({
+                    msg: err.message
+                });
+            }
+            }     
+)
+
+
 router.post('/medical/medicine',
 [
     check("id", "Please enter a valid id").notEmpty(),
@@ -92,6 +227,7 @@ router.post('/medical/medicine',
             }
             
 );
+
 
 router.post('/medical/disease',
 [
@@ -136,6 +272,7 @@ router.post('/medical/disease',
             
 );
 
+
 router.get('/report/pulseRate', async(req, res, next) => {
     const abc = req.query.id
 
@@ -145,6 +282,39 @@ router.get('/report/pulseRate', async(req, res, next) => {
         res.send(data);
     })
 });
+
+
+router.get('/report/bloodPressure', async(req, res, next) => {
+    const abc = req.query.id
+
+    Report.find({$and :[{ patient: abc},{bloodPressure :{$gt:0}}]}, {bloodPressure:1,date:1,_id:0})
+    .sort({date:-1})
+    .limit(5).then(data => {
+        res.send(data);
+    })
+})
+
+
+router.get('/report/bloodSugar', async(req, res, next) => {
+    const abc = req.query.id
+
+    Report.find({$and :[{ patient: abc},{bloodSugar :{$gt:0}}]}, {bloodSugar:1,date:1,_id:0})
+    .sort({date:-1})
+    .limit(5).then(data => {
+        res.send(data);
+    })
+})
+
+
+router.get('/report/temperature', async(req, res, next) => {
+    const abc = req.query.id
+
+    Report.find({$and :[{ patient: abc},{temperature :{$gt:0}}]}, {temperature:1,date:1,_id:0})
+    .sort({date:-1}).limit(5).then(data => {
+        res.send(data);
+    })
+}) 
+
 
 router.get('/medical/medicine', async(req, res, next) => {
     const abc = req.query.id
@@ -156,6 +326,7 @@ router.get('/medical/medicine', async(req, res, next) => {
     })
 });
 
+
 router.get('/medical/disease', async(req, res, next) => {
     const abc = req.query.id
 
@@ -165,4 +336,6 @@ router.get('/medical/disease', async(req, res, next) => {
         res.send(data);
     })
 });
+
+
 module.exports = router;
